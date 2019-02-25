fe7f3d752b15305d9c28a83f20ef0c4f875c9b81
Michael Cuffaro <mike@gwydion>
2019-02-25 18:11:19 -0500

    initial commit of PURL code editor

--- purl-editor.js
+++ purl-editor.js
@@ -1,49 +1,47 @@
-var validateAndSave = function() {
-    // VALIDATION SHOULD BE DONE FIRST
-    editor.save();
-    var code = document.getElementById("code").value;
-    var filename = "Untitled.yml";
-    var download_link = document.createElement("a");
-    download_link.download = filename;
+var validate = function() {
+  // Save the contents of the editor to its associated text area:
+  editor.save();
 
-    // dummy name for the hidden link:
-    download_link.innerHTML = "LINKTITLE";
+  // Extract the code from the text area:
+  var code = document.getElementById("code").value;
 
-    window.URL = window.URL || window.webkitURL;
-
-    var code_blob = new Blob([code], {type:'text/plain'});
-    download_link.href = window.URL.createObjectURL(code_blob);
-
-    download_link.onclick = destroyClickedElement;
-    download_link.style.display = "none";
-    document.body.appendChild(download_link);
-    download_link.click();
+  // Embed it into a POST request and send it to the server for processing:
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() { //Call a function when the state changes.
+    if(request.readyState == 4) {
+      // Request finished. Do processing here.
+      console.log("Status: " + request.status + ", Text: " + request.responseText);
+    }
+  }
+  request.open('POST', 'http://127.0.0.1:5000/validate', true);
+  request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  request.send("code=" + encodeURIComponent(code));
 };
 
 function destroyClickedElement(event) {
-    document.body.removeChild(event.target);
+  document.body.removeChild(event.target);
 }
 
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-    mode: "text/x-yaml",
-    theme: "default",
-    lineNumbers: true,
-    matchBrackets: true,
-    showCursorWhenSelecting: true,
-    extraKeys: {
-        "F11": function(cm) {
-            if (!cm.getOption("fullScreen"))
-                alert("To leave fullscreen mode, press F11 or Esc");
-            cm.setOption("fullScreen", !cm.getOption("fullScreen"));
-        },
-        "Esc": function(cm) {
-            if (cm.getOption("fullScreen"))
-                cm.setOption("fullScreen", false);
-        },
-        Tab: function(cm) {
-            var spaces = Array(cm.getOption("indentUnit") + 1).join(" ");
-            cm.replaceSelection(spaces);
-        },
-        "Ctrl-S": validateAndSave,
-    }
+  mode: "text/x-yaml",
+  theme: "default",
+  lineNumbers: true,
+  matchBrackets: true,
+  showCursorWhenSelecting: true,
+  extraKeys: {
+    "F11": function(cm) {
+      if (!cm.getOption("fullScreen"))
+        alert("To leave fullscreen mode, press F11 or Esc");
+      cm.setOption("fullScreen", !cm.getOption("fullScreen"));
+    },
+    "Esc": function(cm) {
+      if (cm.getOption("fullScreen"))
+        cm.setOption("fullScreen", false);
+    },
+    Tab: function(cm) {
+      var spaces = Array(cm.getOption("indentUnit") + 1).join(" ");
+      cm.replaceSelection(spaces);
+    },
+    "Ctrl-S": validate,
+  }
 });
---
+++ server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import json
+import jsonschema
+import os
+import yaml
+from flask import Flask, request, Response
+
+
+app = Flask(__name__)
+
+pwd = os.path.dirname(os.path.realpath(__file__))
+schemafile = "{}/../purl.obolibrary.org/tools/config.schema.json".format(pwd)
+schema = json.load(open(schemafile))
+
+
+@app.route('/validate', methods=['POST'])
+def validate():
+  if request.form.get('code') is None:
+    return Response(status=400)
+
+  try:
+    yaml_source = yaml.load(request.form['code'])
+    jsonschema.validate(yaml_source, schema)
+  except (yaml.YAMLError, jsonschema.exceptions.ValidationError) as err:
+    return Response(format(err), status=400)
+
+  return Response(status=200)
