c1b8da993a1065ec56e0abd2cae73e22d97ac357
Michael Cuffaro <mike@gwydion>
2019-02-26 17:03:47 -0500

    add validation area to bottom of page

--- purl-editor.js
+++ purl-editor.js
@@ -1,23 +1,3 @@
-var validate = function() {
-  // Save the contents of the editor to its associated text area:
-  editor.save();
-
-  // Extract the code from the text area:
-  var code = document.getElementById("code").value;
-
-  // Embed it into a POST request and send it to the server for processing:
-  var request = new XMLHttpRequest();
-  request.onreadystatechange = function() { //Call a function when the state changes.
-    if(request.readyState == 4) {
-      // Request finished. Do processing here.
-      console.log("Status: " + request.status + ", Text: " + request.responseText);
-    }
-  }
-  request.open('POST', 'http://127.0.0.1:5000/validate', true);
-  request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  request.send("code=" + encodeURIComponent(code));
-};
-
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
   mode: "text/x-yaml",
   theme: "default",
@@ -27,15 +7,10 @@ var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
   extraKeys: {
     "F11": function(cm) {
       if (!cm.getOption("fullScreen")) {
-        bootbox.dialog({
+        bootbox.alert({
           title: 'Entering fullscreen editor mode',
           message: 'To leave fullscreen mode, press F11 or Esc',
           closeButton: false,
-          buttons: {
-            ok: {
-              label: 'Ok',
-            },
-          },
         });
       }
       cm.setOption("fullScreen", !cm.getOption("fullScreen"));
@@ -51,3 +26,40 @@ var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
     "Ctrl-S": validate,
   }
 });
+
+var validate = function() {
+  // Save the contents of the editor to its associated text area:
+  editor.save();
+
+  // Extract the code from the text area:
+  var code = document.getElementById("code").value;
+
+  // Clear the validation area:
+  var validationArea = document.getElementById("validation-area");
+  validationArea.style.color = "#000000";
+  validationArea.innerHTML = "Validating ...";
+
+  // Embed the code into a POST request and send it to the server for processing:
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() { //Call a function when the state changes.
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        validationArea.style.color = "#00CD00";
+        validationArea.innerHTML = "Validation successful";
+        // TODO: Enable the Save button here
+      }
+      else {
+        // Display the error message in the validation area. Note that we must replace any angle
+        // angle brackets with HTML escape codes.
+        alertText = "Validation failed.\n\n" +
+          request.responseText.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        alertText = '<div class="preformatted">' + alertText + '</div>';
+        validationArea.style.color = "#FF0000";
+        validationArea.innerHTML = alertText;
+      }
+    }
+  }
+  request.open('POST', 'http://127.0.0.1:5000/validate', true);
+  request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  request.send("code=" + encodeURIComponent(code));
+};
