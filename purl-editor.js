e81e8727dd85d8fc024dc614f48618a004ce4d55
Michael Cuffaro <mike@gwydion>
2019-03-08 07:42:11 +0100

    add custom autocomplete function

--- purl-editor.js
+++ purl-editor.js
@@ -29,9 +29,108 @@ var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
 });
 
 
-// Add "anyword" hinting to the editor:
+var purlYamlHint = function(editor, options) {
+  /* Generates completion hints depending on the current cursor position of the yaml file */
+  var cursor = editor.getCursor();
+  var thisLine = editor.getLine(cursor.line);
+  var currEnd = cursor.ch;
+  var currStart = currEnd;
+
+  // Look left and right from the current cursor position to have in view the entire word at that
+  // location:
+  while (currStart && /[\w$]+/.test(thisLine.charAt(currStart - 1))) {
+    --currStart;
+  }
+  while (currEnd && /[\w$]+/.test(thisLine.charAt(currEnd))) {
+    ++currEnd;
+  }
+  var currWord = thisLine.slice(currStart, currEnd);
+
+  var currContext = function() {
+    /* Finds the nearest root-level directive above the current line (if one exists) and returns its
+       name. */
+    var lineNum = cursor.line;
+    var matches = /^(\w+):/.exec(editor.getLine(lineNum));
+    while (!matches && lineNum > 0) {
+      matches = /^(\w+):/.exec(editor.getLine(--lineNum));
+    }
+    return matches && matches[1];
+  };
+
+  // The beginning and ending positions of the text that will be replaced if a completion
+  // hint is selected:
+  var from = CodeMirror.Pos(cursor.line, currStart);
+  var to = CodeMirror.Pos(cursor.line, currEnd);
+
+  // If there is no word here just return an empty list:
+  if (currStart === currEnd) {
+    return {list: [], from: from, to: to}
+  }
+
+  var pruneReplacementList = function(replacementList) {
+    /* Prunes the given list of completions to only those which begin with the current word.
+       If none match, then returns the entire list.
+       If there is only one match and it is exact, return nothing.  */
+    prunedList = replacementList.filter(function(r) {
+      return (new RegExp("^" + currWord).test(r['displayText']));
+    });
+    if (!prunedList || prunedList.length === 0) {
+      return replacementList;
+    }
+    else if (prunedList.length === 1 && prunedList[0]['displayText'] === currWord) {
+      return [];
+    }
+    else {
+      return prunedList;
+    }
+  };
+
+  // Send back a completion hint lists contextualised to the current current position as well as to
+  // the letters that have been typed so far.
+  var prevString = thisLine.slice(0, currStart);
+  if (prevString === '') {
+    return {list: pruneReplacementList([{displayText: 'base_redirect: ', text: 'base_redirect: '},
+                                        {displayText: 'base_url:', text: 'base_url: '},
+                                        {displayText: 'entries:', text: 'entries:\n- '},
+                                        {displayText: 'example_terms:', text: 'example_terms:\n- '},
+                                        {displayText: 'idspace:', text: 'idspace: '},
+                                        {displayText: 'products:', text: 'products:\n- '},
+                                        {displayText: 'term_browser:', text: 'term_browser: '},
+                                        {displayText: 'tests:', text: 'tests:\n- from: \n  to: '}]),
+            from: from, to: to};
+  }
+  else if (/^term_browser:\s+$/.test(prevString)) {
+    return {list: pruneReplacementList([{displayText: 'ontobee', text: 'ontobee'},
+                                        {displayText: 'custom', text: 'custom'}]),
+            from: from, to: to};
+  }
+  else if (/^base_url:\s+$/.test(prevString)) {
+    return {list: pruneReplacementList([{displayText: '/obo/', text: '/obo/'}]),
+            from: from, to: to};
+  }
+  else if (/^-\s+$/.test(prevString) && currContext() === 'tests') {
+    return {list: pruneReplacementList([{displayText: 'from:', text: 'from: \n  to: '},
+                                        {displayText: 'to:', text: 'to: '}]),
+            from: from, to: to};
+  }
+  else if (/^-\s+$/.test(prevString) && currContext() === 'entries') {
+    return {list: pruneReplacementList([{displayText: 'exact:', text: 'exact: \n  replacement: '},
+                                        {displayText: 'prefix:', text: 'prefix: \n  replacement: '},
+                                        {displayText: 'regex:', text: 'regex: \n  replacement: '}]),
+            from: from, to: to};
+  }
+  else if (prevString === '  ' && currContext() === 'entries') {
+    return {list: pruneReplacementList([{displayText: 'replacement:', text: 'replacement: '},
+                                        {displayText: 'status:', text: 'status: '},
+                                        {displayText: 'tests:', text: 'tests:\n  - from: \n    to: '}]),
+            from: from, to: to};
+  }
+};
+
+
+// Add our custom hinting function to the editor:
 CodeMirror.commands.autocomplete = function(cm) {
-  cm.showHint({hint: CodeMirror.hint.anyword, completeSingle: false});
+  cm.showHint({hint: purlYamlHint, completeSingle: false});
 }
 
 
@@ -40,10 +139,9 @@ editor.on("keyup", function (cm, event) {
   // If the autocompletion popup is not already active, and if the user has typed a letter,
   // then activate the autocompletion popup:
   if (!cm.state.completionActive) {
-    // 'event.which' is preferred, but IE8 does not support it:
-    var keyPressed = event.which || event.keyCode;
     // Letter keys only:
-    if (keyPressed > 64 && keyPressed < 91){
+    keyPressed = event.key.toLowerCase();
+    if (keyPressed >= 'a' && keyPressed <= 'z') {
       CodeMirror.commands.autocomplete(cm);
     }
   }
