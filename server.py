a3a1382fcaec111e78ec90599db7bc504842a8a6
Michael Cuffaro <mike@gwydion>
2019-03-13 10:48:31 +0100

    WIP find line number of yaml error (seems to be working correctly now)

--- server.py
+++ server.py
@@ -34,10 +34,10 @@ def validate():
     # TODO: THIS NEEDS TO BE SENT BACK AS A JSON JUST AS BELOW:
     return Response(format(err), status=400)
   except jsonschema.exceptions.ValidationError as err:
-    def get_error_start(start, block_label=None, item=None):
+    def get_error_start(start, block_label=None, item=-1):
       print("Searching from line {} for{}block: '{}'"
-            .format(start+1, ' item #{} of '.format(item+1) if item else ' ', block_label))
-      pattern = '^\s*{}\s*:.*$'.format('\w+' if not block_label else block_label)
+            .format(start+1, ' item #{} of '.format(item+1) if item >= 0 else ' ', block_label))
+      pattern = '^\s*{}\s*:.*$'.format('\w+' if not block_label else '-?\s*{}'.format(block_label))
       codelines = code.splitlines()[start:]
       block_start_found = False
       curr_item = 0
@@ -46,31 +46,33 @@ def validate():
           block_start_found = True
           start = start+i
           print("Found the start of the block: '{}' at line {}".format(line, start+1))
-          if not item:
-            break
-        elif block_start_found and item and re.match('\s*-\s*\w+:', line):
+          if item < 0:
+            return start
+        elif block_start_found and item >= 0 and re.match('\s*-\s*\w+:', line):
           print("Found item #{} of block: '{}' at line {}. Line is: '{}'"
                 .format(curr_item+1, block_label, start+i+1, line))
           if curr_item == item:
             start = start+i
-            break
+            return start
           curr_item += 1
 
+      print("*** Hey we shouldn't be here! ***")
       return start
 
+    print("Determining line number for error: {}".format(list(err.absolute_path)))
     start = 0
     if not err.absolute_path:
       start = 1
-      print("Error begins at line {}".format(start+1))
+      print("We know that error begins at line {}".format(start+1))
     else:
       for component in err.absolute_path:
         if type(component) is str:
           block_label = component
           start = get_error_start(start, block_label)
-          print("Error begins at line {}".format(start+1))
+          print("So far we know that error begins at line {}".format(start+1))
         elif type(component) is int:
           start = get_error_start(start, block_label, component)
-          print("Error begins at line {}".format(start+1))
+          print("So far we know that error begins at line {}".format(start+1))
 
     return (jsonify({'summary': format(err.message),
                      'line_number': start+1,
